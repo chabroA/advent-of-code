a5ec1d74749e5bfcbe4748857a779dcab33609c6
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-02-07 13:34:24 +0100

    👔 add day3 part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,15 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
-
+fn part1(lines: &Vec<String>) {
     let mut sum = 0;
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-
+    for line in lines {
         let (first, second) = line.split_at(line.len() / 2);
 
         let mut first = String::from(first);
@@ -19,12 +14,38 @@ fn main() {
 
         if ('a'..='z').contains(&c) {
             sum += c as i32 % 96;
-        }
-
-        if ('A'..='Z').contains(&c) {
+        } else if ('A'..='Z').contains(&c) {
             sum += c as i32 % 64 + 26;
         }
     }
 
-    println!("Sum: {sum}");
+    dbg!(sum);
+}
+
+fn part2(lines: &Vec<String>) {
+    let mut sum = 0;
+
+    for x in (0..lines.len()).step_by(3) {
+        let mut line = String::from(&lines[x]);
+        line.retain(|c| lines[x + 1].contains(c) && lines[x + 2].contains(c));
+
+        let c = line.chars().next().unwrap();
+
+        if ('a'..='z').contains(&c) {
+            sum += c as i32 % 96;
+        } else if ('A'..='Z').contains(&c) {
+            sum += c as i32 % 64 + 26;
+        }
+    }
+
+    dbg!(sum);
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines().collect::<Result<Vec<_>, _>>().unwrap();
+
+    part1(&lines);
+    part2(&lines);
 }
