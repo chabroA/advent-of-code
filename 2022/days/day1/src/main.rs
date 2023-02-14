f6ab9faf7033a2c71ed85707b720a08f35b38aa3
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-02-14 19:21:49 +0100

    ♻️ rework day 1

--- 2022/days/day1/src/main.rs
+++ 2022/days/day1/src/main.rs
@@ -1,28 +1,36 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+
+fn part1() -> u32 {
+    let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
+
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .filter_map(|s| s.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap()
+}
+
+fn part2() -> u32 {
+    let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
+
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .filter_map(|s| s.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum()
+}
 
 fn main() {
-    let file = File::open("inputs/day1.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let mut elf_calories = vec![0];
-    let mut elf_index = 0;
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let calories = line.trim().parse::<i32>().unwrap_or(0);
-        if calories == 0 {
-            elf_index += 1;
-            elf_calories.push(0);
-        } else {
-            elf_calories[elf_index] += calories;
-        }
-    }
-
-    let max_calories = elf_calories.iter().max().unwrap();
-    dbg!(max_calories);
-
-    elf_calories.sort();
-    let top3: i32 = elf_calories.iter().rev().take(3).sum();
-    dbg!(top3);
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
 }
